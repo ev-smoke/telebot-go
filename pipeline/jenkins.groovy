eeeb0df7a531a2b034dcf58d2d241c0923070cd2
ev-smoke <53060147+ev-smoke@users.noreply.github.com>
2025-06-08 15:08:39 +0000

    add stage to the jenkins file

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,5 +1,9 @@
 pipeline {
     agent any
+    environment {
+        REPO = 'https://github.com/ev-smoke/telebot-go'
+        BRANCH = 'main'
+    }
     
     parameters {
         choice(
@@ -22,5 +26,39 @@ pipeline {
             defaultValue: false,
             description: 'Skip running linter'
         )
+    }
+
+    stages {
+        stage ("clone") {
+            steps {
+                echo '>>> Clone repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage ("build") {
+            steps {
+                echo '>>> Build packet'
+                sh 'make build'
+            }
+        }
+
+        stage ("image") {
+            steps {
+                echo '>>> Build image'
+                sh 'make image'
+            }
+        }
+
+        stage ("push") {
+            steps {
+                echo '>>> push image'
+                script {
+                    docker.withRegistry( '', 'dockerhub' ) {
+                        sh 'make push'
+                    }
+                }
+            }
+        }
     }
 }
